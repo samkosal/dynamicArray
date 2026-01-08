256517ee6878d699414abea0c78bf00898b040f6
Jonus <joclapshaw@gmail.com>
2026-01-08 15:12:31 -0800

    Added remove feature

---
+++ src/DynamicDemo.java
@@ -0,0 +1,6 @@
+public class DynamicDemo {
+    public static void main(String[] args){
+        DynamicStringList newList = new DynamicStringList();
+        System.out.println(newList.getLength());
+    }
+}
--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,9 +1,15 @@
 public class DynamicStringList implements StringList {
      
     private String[] strList;
+    private int arrLength;
 
     public DynamicStringList(){
         this.strList = new String[4];
+        this.arrLength = 4;
+    }
+
+    public int getLength(){
+        return arrLength;
     }
 
     @Override
@@ -20,5 +26,28 @@ public class DynamicStringList implements StringList {
     public void add(String value){
         int length = strList.length - 1;
         strList[length] = value;
+    }
+
+    /**
+    * Removes the string at the specified index from the list.
+    *
+    * @param index the index of the string to remove.
+    * @return the string that was removed.
+    * @throws IndexOutOfBoundsException if the index is out of range (index < 0 or index >= size()).
+    */
+    @Override
+    public String remove(int index){
+        String removed = strList[index];
+
+        if(index < strList.length || index > strList.length){
+            throw new IndexOutOfBoundsException(index + "is out of position");
+        }
+
+        for (int i = index; i < strList.length - 1; i++) {
+            strList[i] = strList[i + 1];
+        }
+        this.arrLength--;
+
+        return removed;
     }
 }
